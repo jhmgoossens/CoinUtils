d33d09732ecaaa333a176947ebd5742a2165a201
Jan-Willem <56190178+jhmgoossens@users.noreply.github.com>
2026-01-04 18:02:17 +0100

    Update CoinError.cpp popup blocker

--- src/CoinError.cpp
+++ src/CoinError.cpp
@@ -11,6 +11,7 @@ bool COINUTILSLIB_EXPORT CoinError::printErrors_ = false;
 #ifdef _MSC_VER
 #include <windows.h>
 #include <crtdbg.h>
+#include <stdlib.h>
 COINUTILSLIB_EXPORT
 void WindowsErrorPopupBlocker()
 {
@@ -19,6 +20,7 @@ void WindowsErrorPopupBlocker()
   SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
   _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
   _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
+    _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT)
 }
 #else
 COINUTILSLIB_EXPORT

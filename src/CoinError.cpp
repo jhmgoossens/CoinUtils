c575fb4f3bb608a8552d27972e8554d89751596f
Jan-Willem <56190178+jhmgoossens@users.noreply.github.com>
2026-01-04 18:07:30 +0100

    Update CoinError.cpp typo

--- src/CoinError.cpp
+++ src/CoinError.cpp
@@ -20,7 +20,7 @@ void WindowsErrorPopupBlocker()
   SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
   _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
   _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-    _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT)
+    _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
 }
 #else
 COINUTILSLIB_EXPORT
